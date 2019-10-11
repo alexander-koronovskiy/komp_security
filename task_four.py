da368237815d08df7b0438454cdc2bf1164b7c49
alexander <alexander.koronovskiy@gmail.com>
2019-10-11 10:37:30 +0400

    task 4 test

--- task_four.py
+++ task_four.py
@@ -1,24 +1,26 @@
+def enc(text):
+    s = list()
+    for i in text:
+        s.append(bin(ord(i))[2:].count('1') * '_')
+    return s
+
+
+# чтение исходного текста
 my_file = open("text.txt")
 text = my_file.read()
-j = 0
-i_r = list(); s_r = list()
-for i in range(len(text)):
-    if text[i] == '\n':
-        i_r.append(i)
-        s = 0
-        for k in text[j:i]:
-            s = s + bin(ord(k))[2:].count('1')
-        s_r.append(s)
-        j = i
-print(i_r, s_r)
 
-# добавление количества пробелов в конце каждого
-p = 0  # счетчик вхождений
-j = 0
-s1 = ''  # новая строка
-for i in range(len(text)):
-    if i in i_r:
-        s1 = s1 + text[j:i] + s_r[p]*' '
-        j = i
-        p = p + 1
-print(s1)
+# запись
+s = list()
+for i in text:
+    s.append(bin(ord(i))[2:].count('1')*'_')
+with open(r"lines.txt", "w") as file:
+    file.writelines("%s\n" % line for line in s)
+
+# чтение зашифрованного файла
+with open(r"lines.txt", "r") as file:
+    for line in file:
+        print(line.count('_'))
+
+# кодирование среза text[:]
+# кодирование всего текста
+# чтение зашифрованного файла
