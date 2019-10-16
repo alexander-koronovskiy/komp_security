aac96867293a558f9934cbcf1ec54d145234390a
alexander <alexander.koronovskiy@gmail.com>
2019-10-16 21:45:24 +0400

    before 4 task 01

--- task_four.py
+++ task_four.py
@@ -17,7 +17,7 @@ for i in range(len(text)):
         i_r.append(i)
 
 # шифрование фразы (т.е. скрытой части текста)
-phrase = text[60:68]
+phrase = text[60:62]
 phrase_crypt = enc(phrase)
 
 # добавление шифра в исхдный файл
@@ -31,5 +31,13 @@ if len(phrase_crypt) >= len(i_r):
     blocks = phrase_crypt[p:]
     for k in range(len(blocks)):
         s1 = s1 + '\n' + blocks[k]
-# обработка на случай len(phrase_crypt) < len(i_r)
+if len(phrase_crypt) < len(i_r):
+    for i in range(len(text)):
+        if i in i_r:
+            s1 = s1 + text[j:i] + phrase_crypt[p]
+            j = i
+            p = p + 1
+            if p == len(phrase_crypt):
+                s1 = s1 + text[j:]
+                break
 print(s1)
