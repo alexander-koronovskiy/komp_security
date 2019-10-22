6700deb357dfb760bffe073ec98eb92b07de38ca
alexander <alexander.koronovskiy@gmail.com>
2019-10-22 22:29:15 +0400

    task five version 1.0

--- task_five.py
+++ task_five.py
@@ -9,27 +9,59 @@ def enc(text):
     for i in text:
         symbol_bytes = bin(ord(i))[2:]
         for j in symbol_bytes:
-            colomn = colomn + str(j)
+            colomn += str(j)
     return colomn
 
 
-# зашифрованное в двоичном коде сообщение
+# алгоритм дешифровки
+def decrypt(text):
+    s = ""
+    for i in text:
+        if i in CHARS_SIM.keys():
+            s += "1"
+        if i in CHARS_SIM.values():
+            s += "0"
+    return s
+
+
+# шифрование сообщения
 s = enc(PHRASE)
 
-# все позиции в тексте с похожими символами
+# чтение первоначального текста из файла
 my_file = open("text.txt")
 text = my_file.read()
 
-sym = list()    # символы keys()
-p = list()      # позиции символов
+# шифрование
+text_new = ''
 j = 0
+k = 0
 for i in text:
-    j = j + 1
+    k += 1
     if i in CHARS_SIM.keys():
-        sym.append(i)
-        p.append(j)
+        if s[j] == "0":
+            text_new += CHARS_SIM[i]
+        else:
+            text_new += i
+        j += 1
+        if j == len(s):
+            break
+    else:
+        text_new += i
+text_new += text[k:]
 
-# реализовать алгоритм замены
-# позиции keys
-# позиции values
-#
+# запись результата в файл
+with open(r"index.txt", "w") as file:
+    file.write(text_new)
+
+# чтение из записанного файла
+# my_file_new = open("text.txt")
+# text_new = my_file_new.read()
+
+# дешифровка
+s_new = decrypt(text_new)
+
+symbols = list(map(''.join, zip(*[iter(s_new)] * 11)))
+s1 = ''
+for k in range(len(symbols)):
+    s1 = s1 + chr(int(symbols[k], 2))
+print(s1)
